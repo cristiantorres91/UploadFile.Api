3f60b9652d9818abe330ce035d6864333c01bc20
Cristian Torres <ctorres@codisa.com>
2022-10-16 17:55:00 -0600

    Add project files.

---
+++ UploadFile.Api/Controllers/FilesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UploadFile.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public FilesController(IWebHostEnvironment webHost)
+        {
+            _webHostEnvironment = webHost;
+        }
+
+        [HttpPost("Upload")]
+        public IActionResult Upload(IFormFile file)
+        {
+            try
+            {
+                if (file.Length == 0)
+                    return BadRequest();
+
+                var path = Path.Combine(_webHostEnvironment.ContentRootPath, "Files");
+
+                if(!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                string fullPath = Path.Combine(path, file.FileName);
+                using(var stream = new FileStream(fullPath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+
+                return Ok(file);
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+        }
+    }
+}
